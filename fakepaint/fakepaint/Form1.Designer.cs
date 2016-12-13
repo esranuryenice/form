0b77a916cdbaa6509b50de2c687bdc4956601ce2
esranur.yenice <esranur.yenice@ALS-YG39104-J01>
2016-12-13 10:27:26 +0200

    FakePaint

---
+++ fakepaint/fakepaint/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace fakepaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_renk = new System.Windows.Forms.Button();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "8",
+            "12",
+            "24",
+            "48",
+            "72",
+            "200"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_renk
+            // 
+            this.btn_renk.Location = new System.Drawing.Point(154, 12);
+            this.btn_renk.Name = "btn_renk";
+            this.btn_renk.Size = new System.Drawing.Size(75, 23);
+            this.btn_renk.TabIndex = 1;
+            this.btn_renk.Text = "Renk Seç";
+            this.btn_renk.UseVisualStyleBackColor = true;
+            this.btn_renk.Click += new System.EventHandler(this.btn_renk_Click);
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.Location = new System.Drawing.Point(245, 12);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(75, 23);
+            this.btn_temizle.TabIndex = 2;
+            this.btn_temizle.Text = "Temizle";
+            this.btn_temizle.UseVisualStyleBackColor = true;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 261);
+            this.Controls.Add(this.btn_temizle);
+            this.Controls.Add(this.btn_renk);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_renk;
+        private System.Windows.Forms.Button btn_temizle;
+    }
+}
+
