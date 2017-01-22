464609ac18d40f50551e9e3d3474473c810d9198
esranur.yenice <esranur.yenice@ALS-YG39104-J02>
2017-01-22 15:57:24 +0200

    3

---
+++ SQL_ÇEKME/SQL_ÇEKME/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_ÇEKME
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglan = new SqlConnection("Server=ALS-YG39104-J00;"+"Database=Northwind;"+"UID=sa;PWD=123456789?");
+            SqlDataAdapter dap = new SqlDataAdapter("SELECT*FROM Employees",baglan);
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
